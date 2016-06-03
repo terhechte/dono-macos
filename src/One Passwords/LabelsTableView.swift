8701c47a47bd48d878ce3a6b85b43d38a61beaa5
Panos Sakkos <panos.sakkos@protonmail.com>
2016-06-03 21:34:49 +0200

    checkpoint

---
+++ src/One Passwords/LabelsTableView.swift
@@ -0,0 +1,14 @@
+//
+//  LabelsTableView.swift
+//  Dono
+//
+//  Created by Ghost on 6/3/16.
+//  Copyright © 2016 Panos Sakkos. All rights reserved.
+//
+
+import Cocoa
+
+class LabelsTableView : NSTableRowView
+{
+
+}
