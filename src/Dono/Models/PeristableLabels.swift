a82b3101d0099762deb4cc9a1487a413e0fcf559
Panos Sakkos <panos.sakkos@gmail.com>
2016-07-29 21:21:34 +0200

    v1.3.0
    - Fixes #33

--- src/Dono/Models/PeristableLabels.swift
+++ src/Dono/Models/PeristableLabels.swift
@@ -18,7 +18,7 @@ import Foundation
 
 let docsFolder = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String;
 
-let labelsfilename = "/.service-tags";
+let labelsfilename = "/.labels";
 let pathToServiceTagsFile = docsFolder.stringByAppendingString(labelsfilename);
 
 internal class PersistableLabels
